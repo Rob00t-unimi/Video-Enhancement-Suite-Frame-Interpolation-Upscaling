05cfb13eebf90ba5acd17042def96e17e2d0fcb3
Rob00t-unimi <roberto.tallarini@studenti.unimi.it>
2023-10-29 22:06:40 +0100

    Doppio Bilinear
    Doppio Bilinear su Img 480p con 1.5x di zoom portato quindi a 1080p
    
    ad ogni zoom faccio anche bilateral filter + sharpening
    
    - bilateral riduce il rumore con uno smoothing gaussiano
    
    - sharpening accentua leggermente i bordi

--- demo.py
+++ demo.py
@@ -7,8 +7,8 @@ from BilinearUpscaling import bilinear_upscale
 from visualize import visualizeUpscaledImages
 
 #path dei 2 frame
-framePath1 = "progetto-principi/materials/input/race/frame1-720p.png"
-framePath2 = "progetto-principi/materials/input/race/frame2-720p.png"
+framePath1 = "progetto-principi/materials/input/race/frame1-480p.png"
+framePath2 = "progetto-principi/materials/input/race/frame2-480p.png"
 zoom_factor = 1.5
 
 #upscaling dei frame:
@@ -24,8 +24,10 @@ frame2, up_img2, up_bi_filtered_img2, up_bi_sharp2  = bilinear_upscale(framePath
 ######################
 #frame2, up_img2, up_bi_filtered_img2, up_bi_sharp2  = None, None, None, None
 #provo ad applicarlo di nuovo per vedere se migliora un po' sull'immagine già upcalata e filtrata con il filtro bilaterale
-#cv2.imwrite("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-filtered1.png", up_bi_sharp1)  # Salva l'immagine elaborata su disco
-#frame1, up_img1, up_bi_filtered_img1, up_bi_sharp1 = bilinear_upscale("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-filtered1.png", zoom_factor, 1)
+cv2.imwrite("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-first-filtered1.png", up_bi_sharp1)  # Salva l'immagine elaborata su disco
+tmp1, up_img1, up_bi_filtered_img1, up_bi_sharp1 = bilinear_upscale("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-first-filtered1.png", zoom_factor, 1)
+cv2.imwrite("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-first-filtered2.png", up_bi_sharp2)  # Salva l'immagine elaborata su disco
+tmp2, up_img2, up_bi_filtered_img2, up_bi_sharp2 = bilinear_upscale("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-first-filtered2.png", zoom_factor, 2)
 ######################
 
 
