488aa69c68069a8334f3a6627210a8fd4ec3fca8
Rob00t-unimi <115633385+Rob00t-unimi@users.noreply.github.com>
2023-10-31 12:29:10 +0100

    All fixed
    Eseguire file demo.py

--- demo.py
+++ demo.py
@@ -1,44 +1,9 @@
-import numpy as np
-import scipy
-import cv2
-import matplotlib.pyplot as plt
 from imageProcessing.BicubicUpscaling import bicubic_upscale
 from BilinearUpscaling import bilinear_upscale
 from imageProcessing.visualize import visualizeUpscaledImages
 from videoFrameInterpolation import frameInterpolation
 from videoFrameUpscaling import video_upscaling
-
-# #path dei 2 frame
-# framePath1 = "progetto-principi/materials/input/race/frame1-480p.png"
-# framePath2 = "progetto-principi/materials/input/race/frame2-480p.png"
-# zoom_factor = 1.5
-
-# #upscaling dei frame:
-# #Bilineare
-# frame1, up_img1, up_bi_filtered_img1, up_bi_sharp1  = bilinear_upscale(framePath1, zoom_factor, 1)
-# frame2, up_img2, up_bi_filtered_img2, up_bi_sharp2  = bilinear_upscale(framePath2, zoom_factor, 2)
-
-# #Bicubico
-# #frame1, up_img1, up_bi_filtered_img1, up_bi_sharp1 = bicubic_upscale(framePath1, zoom_factor, 1)
-# #frame2, up_img2, up_bi_filtered_img2, up_bi_sharp2 = bicubic_upscale(framePath2, zoom_factor, 2)
-
-
-# ######################
-# #frame2, up_img2, up_bi_filtered_img2, up_bi_sharp2  = None, None, None, None
-# #provo ad applicarlo di nuovo per vedere se migliora un po' sull'immagine già upcalata e filtrata con il filtro bilaterale
-# cv2.imwrite("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-first-filtered1.png", up_bi_sharp1)  # Salva l'immagine elaborata su disco
-# tmp1, up_img1, up_bi_filtered_img1, up_bi_sharp1 = bilinear_upscale("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-first-filtered1.png", zoom_factor, 1)
-# cv2.imwrite("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-first-filtered2.png", up_bi_sharp2)  # Salva l'immagine elaborata su disco
-# tmp2, up_img2, up_bi_filtered_img2, up_bi_sharp2 = bilinear_upscale("progetto-principi/materials/output/upscaling/bilinear/img_upscaledBilinear-first-filtered2.png", zoom_factor, 2)
-# ######################
-
-
-# print("Completato!")
-# #visualizzazione frame upscalati
-# #originale - upscalato - upscalato con bilateral filter - upscalato con bilateral filter e sharpening filter
-# visualizeUpscaledImages(frame1, up_img1, up_bi_filtered_img1, up_bi_sharp1, frame2, up_img2, up_bi_filtered_img2, up_bi_sharp2)
-
-# print("Continuo..")
+import msvcrt
 
 #Video Upscaling
 input_video_path = "materials/stockVideos/short_480p_10fps.mp4"  # Sostituisci con il percorso del tuo video di input
@@ -48,7 +13,15 @@ iterazioniUpscaling = 1 # numero di volte in cui viene eseguito l'upscaling sull
 
 print("Starting Upscaling...")
 video_upscaling(input_video_path, output_video_path, zoom_factor, iterazioniUpscaling)
+print("New Video There: materials/output/VideoProcessing/VideoUpscaling/upscaledVideo.avi")
+print("Press Enter to continue...")
 
+# Attendi fino a quando viene premuto "Enter"
+while True:
+    if msvcrt.kbhit():
+        key = msvcrt.getch()
+        if key == b'\r':
+            break
 
 #frame interpolation
 inputVideoPath = "materials/output/VideoProcessing/VideoUpscaling/upscaledVideo.avi"
@@ -56,5 +29,6 @@ outputVideoPath = "materials/output/VideoProcessing/FrameInterpolation/Upscaled-
 
 print("Starting Frame Interpolation...")
 frameInterpolation(inputVideoPath, outputVideoPath)
+print("Final Video There: materials/output/VideoProcessing/VideoUpscaling/upscaledVideo.avi")
 
 print("End of System")
