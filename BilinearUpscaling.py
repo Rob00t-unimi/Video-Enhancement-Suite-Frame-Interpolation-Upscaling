58f0e916fb500b95cc02ea217cdbc238c24c3041
Rob00t-unimi <roberto.tallarini@studenti.unimi.it>
2023-11-26 20:51:46 +0100

    repo clean

--- BilinearUpscaling.py
+++ BilinearUpscaling.py
@@ -51,8 +51,8 @@ def bilinear_upscale (frame, zoom_factor):
     def cv2Upscale(img, zoom_factor):
         new_width = int(img.shape[1] * zoom_factor)
         new_height = int(img.shape[0] * zoom_factor)
-        upscaled_image = cv2.resize(img, (new_width, new_height))
-        #upscaled_image = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)     # --> Bicubico di cv2 è più preciso
+        upscaled_image = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+        # upscaled_image = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)     # --> Bicubico di cv2 è più preciso
         return upscaled_image
     
     upscaledImage = cv2Upscale(frame, zoom_factor)
