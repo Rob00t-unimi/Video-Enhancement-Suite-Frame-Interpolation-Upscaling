6d16660e8d54ecb6c5762110d22b017140fade9a
Rob00t-unimi <roberto.tallarini@studenti.unimi.it>
2023-12-01 01:32:41 +0100

    lib update

--- dependeciesInstallation.py
+++ dependeciesInstallation.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Lista delle librerie da installare
-libraries_to_install = ['opencv-python', 'numpy', 'matplotlib', 'ttkbootstrap', 'ttkthemes']
+libraries_to_install = ['opencv-python', 'numpy', 'matplotlib', 'ttkbootstrap', 'ttkthemes', 'imageio']
 
 # Installa le librerie usando pip
 for library in libraries_to_install:
